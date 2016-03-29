9c465793ce905f8e569b58f49591f6206a827846
shuai.chen <shuaichen_ax@163.com>
2016-03-29 16:53:19 +0800

    UnityNative  IOSNative  takePhoto Change

--- Unity/UnityDemo/Assets/Plugins/GF.Native/IOS/IOSNativeAPI.cs
+++ Unity/UnityDemo/Assets/Plugins/GF.Native/IOS/IOSNativeAPI.cs
@@ -13,25 +13,29 @@ public class IOSNativeAPI : INativeAPI
     }
 
     //-------------------------------------------------------------------------
-    public void takeNewPhoto(int photo_width, int photo_height, string store_photopath)
+    public void takeNewPhoto(int photo_width, int photo_height, string photo_name)
     {
-        takeNewPhoto_ios(photo_width, photo_height, store_photopath);
+        takeNewPhoto_ios("NativeAPIMsgReceiver", "getPicSuccess",
+            "getPicFail", photo_width, photo_height, photo_name);
     }
 
     //-------------------------------------------------------------------------
-    public void takeExistPhoto(int photo_width, int photo_height, string store_photopath)
+    public void takeExistPhoto(int photo_width, int photo_height, string photo_name)
     {
-        takeExistPhoto_ios(photo_width, photo_height, store_photopath);
+        takeExistPhoto_ios("NativeAPIMsgReceiver", "getPicSuccess",
+            "getPicFail", photo_width, photo_height, photo_name);
     }
 
-#region DllImport
+    #region DllImport
     //-------------------------------------------------------------------------
     [DllImport("__Internal")]
     private static extern void pay_ios(string charge_data);
     [DllImport("__Internal")]
-    private static extern void takeNewPhoto_ios(int photo_width, int photo_height, string store_photopath);
+    private static extern void takeNewPhoto_ios(string msg_recivername, string take_photosuccessmsgname,
+        string take_photofailmsgname, int photo_width, int photo_height, string store_photopath);
     [DllImport("__Internal")]
-    private static extern void takeExistPhoto_ios(int photo_width, int photo_height, string store_photopath);
-#endregion
+    private static extern void takeExistPhoto_ios(string msg_recivername, string take_photosuccessmsgname,
+        string take_photofailmsgname, int photo_width, int photo_height, string store_photopath);
+    #endregion
 }
 #endif
