70634280b4b3133d09723c9db2b21c8d4b579282
shuai.chen <shuaichen_ax@163.com>
2016-04-05 16:31:30 +0800

    UnityNative   AddDataEye The Directory structure changes

--- Unity/UnityDemo/Assets/GF.Native/NativeSDK.cs
+++ Unity/UnityDemo/Assets/GF.Native/NativeSDK.cs
@@ -93,4 +93,35 @@ public class NativeSDK
     {
         return mINativeAPI.getCurrentMusicVolume();
     }
+
+    //-------------------------------------------------------------------------
+    /// <summary>
+    /// DataEye统计初始化
+    /// </summary>
+    /// <param name="app_id"></param>
+    /// <param name="channel_id"></param>
+    /// <param name="report_mode"></param>
+    public static void initWithAppIdAndChannelId(string app_id, string channel_id, DCReportMode report_mode = DCReportMode.DC_AFTER_LOGIN)
+    {
+        DCAgent.getInstance().initWithAppIdAndChannelId(app_id, channel_id);
+    }
+
+    //-------------------------------------------------------------------------
+    /// <summary>
+    /// DataEye登陆 
+    /// </summary>
+    /// <param name="acc_id"></param>
+    public static void login(string acc_id)
+    {
+        DCAccount.login(acc_id);
+    }
+
+    //-------------------------------------------------------------------------
+    /// <summary>
+    /// DataEye登出
+    /// </summary>
+    public static void logout()
+    {
+        DCAccount.logout();
+    }
 }
