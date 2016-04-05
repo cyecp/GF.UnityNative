56bdb59481f0a197f9a4a39e2e29d162e7b57184
shuai.chen <shuaichen_ax@163.com>
2016-04-05 17:14:36 +0800

    UnityNative  AndroidNative Pay Change

--- Android/AndroidNative/src/com/Pay/Pay/Pay.java
+++ Android/AndroidNative/src/com/Pay/Pay/Pay.java
@@ -1,6 +1,7 @@
 package com.Pay.Pay;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.app.Activity;
 
@@ -9,9 +10,10 @@ import com.AndroidToUnityMsgBridge.AndroidToUnityMsgBridge.AndroidToUnityMsgBrid
 public class Pay {
 	// -------------------------------------------------------------------------
 	private static Pay mPay;
-	private AndroidToUnityMsgBridge mAndroidToUnityMsgBridge;
+	private static AndroidToUnityMsgBridge mAndroidToUnityMsgBridge;
 	private Activity mUnityActivity;
 	private static final int REQUEST_CODE_PAYMENT = 1;
+	private static String UnityPayResultMethord = "PayResult";
 
 	// -------------------------------------------------------------------------
 	public static Pay Instance(String pay_resultreceiver) {
@@ -24,25 +26,27 @@ public class Pay {
 
 	// -------------------------------------------------------------------------
 	private Pay(String pay_resultreceiver) {
-		this.mAndroidToUnityMsgBridge = AndroidToUnityMsgBridge
+		mAndroidToUnityMsgBridge = AndroidToUnityMsgBridge
 				.Instance(pay_resultreceiver);
-		this.mUnityActivity = this.mAndroidToUnityMsgBridge.getActivity();
+		this.mUnityActivity = mAndroidToUnityMsgBridge.getActivity();
 	}
 
 	// -------------------------------------------------------------------------
 	public void pay(String pay_chargedata) {
 		Intent intent = new Intent(this.mUnityActivity,
-				com.pingplusplus.android.PaymentActivity.class);
+				com.Pay.Pay.PayActivity.class);
 		Log.e("Pay", "pay1::" + pay_chargedata);
-		intent.putExtra(com.pingplusplus.android.PaymentActivity.EXTRA_CHARGE,
-				pay_chargedata);
+		intent.putExtra("Pay", pay_chargedata);
 		Log.e("Pay", "pay2::" + pay_chargedata);
-		this.mUnityActivity
-				.startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+		this.mUnityActivity.startActivity(intent);
 		Log.e("Pay", "pay3::" + pay_chargedata);
-
-		this.mAndroidToUnityMsgBridge.sendMsgToUnity("test",
-				"Pay::Success!!!!!!");
 	}
 
+	// -------------------------------------------------------------------------
+	public static void sendToUnityPic(Boolean is_success) {
+		Log.e("Pay", "sendToUnityPic");
+
+		mAndroidToUnityMsgBridge.sendMsgToUnity(UnityPayResultMethord,
+				is_success ? "1" : "0");
+	}
 }
