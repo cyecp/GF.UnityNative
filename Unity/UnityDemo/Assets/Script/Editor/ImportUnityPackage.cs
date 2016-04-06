f9d620eb817bcf6117300b10fb87d2e2fb270017
shuai.chen <shuaichen_ax@163.com>
2016-04-06 11:34:05 +0800

    UnityNative AndroidNative Eclipse Change

---
+++ Unity/UnityDemo/Assets/Script/Editor/ImportUnityPackage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ImportUnityPackage : EditorWindow
+{
+    //-------------------------------------------------------------------------
+    static string mPackagePath;
+
+    //-------------------------------------------------------------------------
+    [MenuItem("ImportUnityPackage/Import")]
+    static void AddWindow()
+    {
+        EditorWindow.GetWindow(typeof(ImportUnityPackage));
+
+        string current_dir = System.Environment.CurrentDirectory;
+        Debug.LogError(current_dir);
+        //\ThirdPackage
+                
+    }
+}
