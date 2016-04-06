417bf65d9cfd84309ea5fe11fa1a6a95c1e9f048
shuai.chen <shuaichen_ax@163.com>
2016-04-06 12:00:42 +0800

    UnityNative  add importUnityPackage

--- Unity/UnityDemo/Assets/Script/Editor/ImportUnityPackage.cs
+++ Unity/UnityDemo/Assets/Script/Editor/ImportUnityPackage.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.IO;
 
 public class ImportUnityPackage : EditorWindow
 {
     //-------------------------------------------------------------------------
     static string mPackagePath;
+    const string mThirdPackageDirectoryName = "ThirdPackage";
 
     //-------------------------------------------------------------------------
     [MenuItem("ImportUnityPackage/Import")]
@@ -14,8 +16,35 @@ public class ImportUnityPackage : EditorWindow
         EditorWindow.GetWindow(typeof(ImportUnityPackage));
 
         string current_dir = System.Environment.CurrentDirectory;
-        Debug.LogError(current_dir);
-        //\ThirdPackage
-                
+        mPackagePath = string.Format(current_dir.Substring(0, current_dir.LastIndexOf(@"\")) + "{0}", @"\" + mThirdPackageDirectoryName);
     }
+
+    //-------------------------------------------------------------------------
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("UnityPackagePath:", mPackagePath);
+        bool import = GUILayout.Button("ImportPackage", GUILayout.Width(200));
+        if (import)
+        {
+            _importPackage();
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    void _importPackage()
+    {
+        if (!Directory.Exists(mPackagePath))
+        {
+            ShowNotification(new GUIContent("无该目录!"));
+        }
+        else
+        {
+            string[] files = Directory.GetFiles(mPackagePath);
+            foreach (var i in files)
+            {
+                AssetDatabase.ImportPackage(i, false);
+
+            }
+        }
+    }    
 }
