55b960cc4d2b918e29460fb4ba18c3f9e1a6c126
shuai.chen <shuaichen_ax@163.com>
2016-04-06 17:06:54 +0800

    UnityNative  ImportUnityPackage

--- Unity/UnityDemo/Assets/Script/Editor/ImportUnityPackage.cs
+++ Unity/UnityDemo/Assets/Script/Editor/ImportUnityPackage.cs
@@ -7,7 +7,16 @@ public class ImportUnityPackage : EditorWindow
 {
     //-------------------------------------------------------------------------
     static string mPackagePath;
+    static string mManifestBackUpPath;
+    static string mCurrentDir;
     const string mThirdPackageDirectoryName = "ThirdPackage";
+    const string mManifestBackUp = "ManifestBackUp";
+    const string mPluginsName = "Plugins";
+    const string mAssetName = "Assets";
+    const string mUnityNativeThirdPackageFilePath = @"Plugins\GF.Native\TheThirdSDK\";
+    const string mPluginsAndoridManifestPath = @"Plugins\Android\AndroidManifest.xml";
+    const string mMainManifestBakUpName = @"\MainManifest\";
+    string[] mThirdPackageName = { "DataEye" };
 
     //-------------------------------------------------------------------------
     [MenuItem("ImportUnityPackage/Import")]
@@ -15,14 +24,18 @@ public class ImportUnityPackage : EditorWindow
     {
         EditorWindow.GetWindow(typeof(ImportUnityPackage));
 
-        string current_dir = System.Environment.CurrentDirectory;
-        mPackagePath = string.Format(current_dir.Substring(0, current_dir.LastIndexOf(@"\")) + "{0}", @"\" + mThirdPackageDirectoryName);
+        mCurrentDir = System.Environment.CurrentDirectory;
+        string current_dir = mCurrentDir;
+        current_dir = current_dir.Substring(0, current_dir.LastIndexOf(@"\"));
+        mPackagePath = string.Format(current_dir + "{0}", @"\" + mThirdPackageDirectoryName);
+        mManifestBackUpPath = string.Format(current_dir + "{0}", @"\" + mManifestBackUp);
     }
 
     //-------------------------------------------------------------------------
     void OnGUI()
     {
         EditorGUILayout.LabelField("UnityPackagePath:", mPackagePath);
+        EditorGUILayout.LabelField("ManifestBackUpPath:", mManifestBackUpPath);
         bool import = GUILayout.Button("ImportPackage", GUILayout.Width(200));
         if (import)
         {
@@ -40,11 +53,77 @@ public class ImportUnityPackage : EditorWindow
         else
         {
             string[] files = Directory.GetFiles(mPackagePath);
+            if (files.Length == 0)
+            {
+                return;
+            }
+
+            _copyFile(_getManifestPath(), mManifestBackUpPath + mMainManifestBakUpName + "AndroidManifest.xml");
+
             foreach (var i in files)
             {
                 AssetDatabase.ImportPackage(i, false);
+                //foreach (var item in mThirdPackageName)
+                //{
+                //    if (i.Contains(item))
+                //    {
+                //        _copyFile(_getManifestPath(), mManifestBackUpPath + @"\" + item + @"\AndroidManifest.xml");
+                //        break;
+                //    }
+                //}
 
+                //_copyToNativePath();
             }
         }
-    }    
+    }
+
+    //-------------------------------------------------------------------------
+    void _copyFile(string source_path, string target_path)
+    {
+        File.Copy(source_path, target_path, true);
+    }
+
+    //-------------------------------------------------------------------------
+    void _copyToNativePath()
+    {
+        string current_dir = mCurrentDir + "/" + mAssetName;
+        string[] dirs = Directory.GetDirectories(current_dir);
+        foreach (var i in dirs)
+        {
+            Debug.LogError(i);
+            foreach (var item in mThirdPackageName)
+            {
+                if (i.Contains(item))
+                {
+                    string last_dir = i.Substring(i.LastIndexOf(@"\") + 1);
+                    string[] files = Directory.GetFiles(i);
+                    foreach (var file in files)
+                    {
+                        string file_name = Path.GetFileName(file);
+                        _copyFile(file, _getThirdPackagePath() + last_dir + "/" + file_name);
+                    }
+                    Directory.Delete(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    string _getManifestPath()
+    {
+        string streaming_assets = Application.streamingAssetsPath;
+        streaming_assets = streaming_assets.Substring(0, streaming_assets.LastIndexOf("/"));
+
+        return streaming_assets + "/" + mPluginsAndoridManifestPath;
+    }
+
+    //-------------------------------------------------------------------------
+    string _getThirdPackagePath()
+    {
+        string streaming_assets = Application.streamingAssetsPath;
+        streaming_assets = streaming_assets.Substring(0, streaming_assets.LastIndexOf("/"));
+
+        return streaming_assets + "/" + mUnityNativeThirdPackageFilePath;
+    }
 }
