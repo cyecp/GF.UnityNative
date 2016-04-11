9d9a3e4085ac69659d6037a02864124154ccea35
shuai.chen <shuaichen_ax@163.com>
2016-04-11 10:34:58 +0800

    UnityNative  Directory changed Add ExportPackageEditor

---
+++ Unity/UnityDemo/Assets/GF.Native.Internal/Editor/EditorGFNativeInternal.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class EditorGFNativeInternal : EditorWindow
+{
+    static string mUnityPackagePath = "";
+
+    //-------------------------------------------------------------------------
+    static EditorGFNativeInternal()
+    {
+        string unity_path = System.Environment.CurrentDirectory;
+        mUnityPackagePath = unity_path.Replace(@"Unity\UnityDemo", "Package");
+    }
+
+    //-------------------------------------------------------------------------
+    [MenuItem("GF/GF.Native/导出UnityNativePackageAll")]
+    static void exportGFUnityPackage()
+    {
+        string[] arr_assetpathname = new string[3];
+        arr_assetpathname[0] = "Assets/GF.Native";
+        arr_assetpathname[1] = "Assets/Plugins";
+        arr_assetpathname[2] = "Assets/Test";
+
+        AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "\\UnityNativePackageAll.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export UnityNativePackageAll.unitypackage Finished!");
+    }
+
+    //-------------------------------------------------------------------------
+    [MenuItem("GF/GF.Native/导出UnityNativeDataEye")]
+    static void exportGFJsonPackage()
+    {
+        string[] arr_assetpathname = new string[4];
+        arr_assetpathname[0] = "Assets/GF.Native";
+        arr_assetpathname[1] = "Assets/Plugins/Android";
+        arr_assetpathname[2] = "Assets/Plugins/GF.Native/DataEye";
+        arr_assetpathname[3] = "Assets/Plugins/GF.Native/SDKReceiveMono";
+        AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "\\UnityNativeDataEye.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export UnityNativeDataEye.unitypackage Finished!");
+    }
+
+    //-------------------------------------------------------------------------
+    [MenuItem("GF/GF.Native/导出UnityNativePay")]
+    static void exportUnityNativePay()
+    {
+        string[] arr_assetpathname = new string[4];
+        arr_assetpathname[0] = "Assets/GF.Native";
+        arr_assetpathname[1] = "Assets/Plugins/Android";
+        arr_assetpathname[2] = "Assets/Plugins/GF.Native/Pay";
+        arr_assetpathname[3] = "Assets/Plugins/GF.Native/SDKReceiveMono";
+        AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "\\UnityNativePay.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export UnityNativePay.unitypackage Finished!");
+    }
+
+    //-------------------------------------------------------------------------
+    [MenuItem("GF/GF.Native/导出GFNativeBySelf")]
+    static void exportGFNativeBySelf()
+    {
+        string[] arr_assetpathname = new string[4];
+        arr_assetpathname[0] = "Assets/GF.Native";
+        arr_assetpathname[1] = "Assets/Plugins/Android";
+        arr_assetpathname[2] = "Assets/Plugins/GF.Native/Native";
+        arr_assetpathname[3] = "Assets/Plugins/GF.Native/SDKReceiveMono";
+        AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "\\GFNativeBySelf.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export GFNativeBySelf.unitypackage Finished!");
+    }
+}
